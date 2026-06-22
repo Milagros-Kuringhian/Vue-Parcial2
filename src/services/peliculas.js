cc1c5b7edf94e8bf9b3d0a6fffb940c79ff0e6ed
Milagros Sol Kuringhian <milagros.kuringhian@davinci.edu.ar>
2026-06-22 19:24:36 -0300

    -finalizado

--- src/services/peliculas.js
+++ src/services/peliculas.js
@@ -1,13 +1,9 @@
-const API_KEY = import.meta.env.VITE_TMDB_API_KEY
+const API_KEY = '4c721763d28c82efc14ef0eb591a347f'
 const BASE_URL = 'https://api.themoviedb.org/3'
 const IMG_POSTER = 'https://image.tmdb.org/t/p/w500'
 const IMG_BACKDROP = 'https://image.tmdb.org/t/p/w1280'
 
 function pedir(endpoint) {
-  if (!API_KEY) {
-    return Promise.reject(new Error('Falta la API key. Creá un archivo .env con VITE_TMDB_API_KEY'))
-  }
-
   const url = `${BASE_URL}${endpoint}&api_key=${API_KEY}&language=es-ES`
   return fetch(url).then(function (response) {
     if (!response.ok) {
